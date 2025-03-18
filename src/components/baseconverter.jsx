5208d0c2a66b5e710ba6e755ac54179967f8e6ce
Joshua Whynot <107290890+Joshua-Whynot@users.noreply.github.com>
2025-03-18 12:41:39 -0400

    Update baseconverter.jsx
    added 1000 to increment options

--- src/components/baseconverter.jsx
+++ src/components/baseconverter.jsx
@@ -31,6 +31,7 @@ const BaseConverter = () => {
         { name: '25', value: 25 },
         { name: '50', value: 50 },
         { name: '100', value: 100 },
+        { name: '1000', value: 1000},
     ]
     
     // Update conversion when count or bases change
